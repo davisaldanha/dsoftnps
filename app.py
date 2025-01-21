9338db3ed939e068b2e4c5d0ac09c4802c25052a
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-01-21 15:17:32 -0300

    Atualização page satisfaction
    Modificado os input number para input radio

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 import smtplib
 import schedule
 import sqlite3
@@ -28,7 +28,7 @@ def add_answers(email, answers):
         ''', (email, *answers))
         conn.commit()
     except sqlite3.IntegrityError:
-        print(f"O Email {email} já enviou repostas.")
+        return redirect(url_for('already_submitted'))
     finally:
         conn.close()
 
@@ -37,15 +37,28 @@ app = Flask(__name__)
 #Rota para a página de satisfação do cliente
 @app.route('/satisfacao', methods=['GET', 'POST'])
 def satisfaction():
+    questions = [
+        "Qual a probabilidade de você recomendar nossa loja para um amigo ou colega?",
+        "Como você avalia a qualidade dos nossos produtos?",
+        "Como você avalia o atendimento que recebeu?",
+        "A rapidez no atendimento atendeu suas expectativas?",
+        "O preço dos produtos é justo?",
+        "A variedade de produtos foi satisfatória?",
+        "O ambiente da loja é agradável?",
+        "Você encontrou tudo o que procurava?",
+        "Como você avalia a facilidade de pagamento?",
+        "Considera que nossos serviços têm boa relação custo-benefício?"
+    ]
+
     if request.method == 'POST':
         email = request.form.get("email")
-        answers = [
+        answers_list = [
             int(request.form.get(f"p{i}")) for i in range(1, 11)
         ]
-        if email and all(answers):
-           add_answers(email, answers)
-           return redirect(url_for('satisfacao'))
-    return render_template('satisfaction.html')
+        if email and all(answers_list):
+           add_answers(email, answers_list)
+           return redirect(url_for('satisfaction'))
+    return render_template('satisfaction.html', questions=questions, enumerate=enumerate)
 
 
 
