a325055d0e929793d41c2b8b12f275a69b55a2a6
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-01-23 14:51:12 -0300

    Inclusão campo telefone
    Incluido o campo telefone na page e no schema do banco de dados

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import smtplib
 import schedule
 import sqlite3
 
+#Excluir banco de dados
+def drop_database():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE satisfaction')
+    conn.commit()
+    conn.close()
+
 #Configuração do banco de dados
 def configure_database():
     conn = sqlite3.connect('database.db')
@@ -11,6 +19,7 @@ def configure_database():
         CREATE TABLE IF NOT EXISTS satisfaction(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             email TEXT NOT NULL UNIQUE,
+            phone TEXT NOT NULL,
             p1 INTEGER, p2 INTEGER, p3 INTEGER, p4 INTEGER, p5 INTEGER,
             p6 INTEGER, p7 INTEGER, p8 INTEGER, p9 INTEGER, p10 INTEGER
         )
@@ -29,7 +38,7 @@ def add_answers(email, answers):
         conn.commit()
     except sqlite3.IntegrityError:
         return redirect(url_for('already_submitted'))
-    finally:
+    finally: 
         conn.close()
 
 app = Flask(__name__)
@@ -52,11 +61,12 @@ def satisfaction():
 
     if request.method == 'POST':
         email = request.form.get("email")
+        phone = request.form.get("phone")
         answers_list = [
             int(request.form.get(f"p{i}")) for i in range(1, 11)
         ]
-        if email and all(answers_list):
-           add_answers(email, answers_list)
+        if email and phone and all(answers_list):
+           add_answers(email, phone, answers_list)
            return redirect(url_for('already_submitted'))
     return render_template('satisfaction.html', questions=questions, enumerate=enumerate)
 
@@ -66,6 +76,7 @@ def already_submitted():
     return render_template('already_submitted.html')
 
 if __name__ == '__main__':
+    drop_database()
     configure_database()
     app.run(debug=True)
 
